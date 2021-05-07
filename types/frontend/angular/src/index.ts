2fd3ad1b821753dc588904df55bccd6970b1dee9
Germano Loos <germanoloos@hotmail.com>
2021-05-07 19:50:09 -0300

    Build de teste para o angular

--- types/frontend/angular/src/index.ts
+++ types/frontend/angular/src/index.ts
@@ -1,7 +1,7 @@
 import { AngurooProject } from "./model/project.model";
 import payload = require("../payload.json");
 
-const angurooProject = payload as AngurooProject;
+const angurooProject = payload as unknown as AngurooProject;
 console.log(angurooProject.name);
 
 console.log("Angular Rules");
