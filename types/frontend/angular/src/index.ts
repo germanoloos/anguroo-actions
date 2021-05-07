782815ab60bb6aee72dd4a8b9739c66ebb1199ca
Germano Loos <germanoloos@hotmail.com>
2021-05-07 17:15:22 -0300

    Build de teste para o angular

--- types/frontend/angular/src/index.ts
+++ types/frontend/angular/src/index.ts
@@ -1,9 +1,9 @@
 import { AngurooProject } from "./model/project.model";
 
-let payload = process.argv.splice(2)[0] as string;
-if (payload && payload !== '') {
-    const angurooProject = JSON.parse(payload) as AngurooProject; 
-    console.log(angurooProject.name);
-}
+// let payload = process.argv.splice(2)[0] as string;
+// if (payload && payload !== '') {
+//     const angurooProject = JSON.parse(payload) as AngurooProject; 
+//     console.log(angurooProject.name);
+// }
 
 console.log("Angular Rules!");
