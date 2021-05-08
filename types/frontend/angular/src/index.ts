5e270c01accb2e71234ceea51a03df90b4d5ef28
Germano Loos <germanoloos@hotmail.com>
2021-05-07 22:36:11 -0300

    Build de teste para o angular

--- types/frontend/angular/src/index.ts
+++ types/frontend/angular/src/index.ts
@@ -1,7 +1,6 @@
 import { AngurooProject } from "./model/project.model";
 
 const data = process.argv.slice(2)[0];
-console.log(new Buffer(data, "base64").toString("ascii"));
+const angurooProject = JSON.parse(new Buffer(data, "base64").toString("ascii")) as AngurooProject;
 
-
-console.log("Angular Rules");
+console.log("Angular Rules! ", angurooProject.name);
