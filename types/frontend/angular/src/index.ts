d49a106da726c95fcbdcde70620a1163c69fc01c
Germano Loos <germanoloos@hotmail.com>
2021-05-18 19:04:02 -0300

    Build de teste para o angular

--- types/frontend/angular/src/index.ts
+++ types/frontend/angular/src/index.ts
@@ -1,6 +1,35 @@
 import { AngurooProject } from "./model/project.model";
 
+const { spawn } = require('child_process');
 const data = process.argv.slice(2)[0];
-const angurooProject = JSON.parse(new Buffer(data, "base64").toString("ascii")) as AngurooProject;
+const angurooProject = JSON.parse(Buffer.from(data, "base64").toString("ascii")) as AngurooProject;
 
-console.log("Angular Rules! ", angurooProject.name);
+// console.log(angurooProject);
+
+const ngCreateCommand = `ng new ${angurooProject.name}` +
+    `--routing=true --skip-install --interactive=false --strict=true` +
+    `--style=${angurooProject.framework.props.stylesheet.toLowerCase()} ` +
+    `--prefix=${angurooProject.framework.props.prefix}`;
+
+console.log(ngCreateCommand);
+
+var isWin = process.platform === "win32";
+let cmdTerminal;
+
+if (isWin) {
+    cmdTerminal = spawn('cmd', ['/c', ngCreateCommand]);
+} else {
+    cmdTerminal = spawn(ngCreateCommand);
+}
+
+cmdTerminal.stdout.on('data', (data: any) => {
+    console.log(`stdout: ${data}`);
+});
+
+cmdTerminal.stderr.on('data', (data: any) => {
+    console.log(`stderr: ${data}`);
+});
+
+cmdTerminal.on('close', (code: any) => {
+    console.log(`child process exited with code ${code}`);
+});
