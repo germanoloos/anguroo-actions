505c1d466c8ca9601c74173f424c216ae613f2ca
Germano Loos <germanoloos@hotmail.com>
2021-05-19 10:54:48 -0300

    Execução de linhas de comando pelo node

--- types/frontend/angular/src/index.ts
+++ types/frontend/angular/src/index.ts
@@ -1,35 +1,20 @@
 import { AngurooProject } from "./model/project.model";
+import { runCommand } from "./services/commands.service";
 
-const { spawn } = require('child_process');
 const data = process.argv.slice(2)[0];
 const angurooProject = JSON.parse(Buffer.from(data, "base64").toString("ascii")) as AngurooProject;
 
-// console.log(angurooProject);
+console.log(angurooProject);
 
-const ngCreateCommand = `ng new ${angurooProject.name}` +
-    `--routing=true --skip-install --interactive=false --strict=true` +
+const angularCliCommand = `npm i -g @angular/cli`;
+const ngCreateCommand = `ng new ${angurooProject.name} ` +
+    `--routing=true --skip-install --interactive=false --strict=true ` +
     `--style=${angurooProject.framework.props.stylesheet.toLowerCase()} ` +
     `--prefix=${angurooProject.framework.props.prefix}`;
+const angularMaterialCommand = `pushd ${angurooProject.name} && ng add @angular/material --skip-confirmation && popd`;
 
-console.log(ngCreateCommand);
-
-var isWin = process.platform === "win32";
-let cmdTerminal;
-
-if (isWin) {
-    cmdTerminal = spawn('cmd', ['/c', ngCreateCommand]);
-} else {
-    cmdTerminal = spawn(ngCreateCommand);
-}
-
-cmdTerminal.stdout.on('data', (data: any) => {
-    console.log(`stdout: ${data}`);
-});
-
-cmdTerminal.stderr.on('data', (data: any) => {
-    console.log(`stderr: ${data}`);
-});
-
-cmdTerminal.on('close', (code: any) => {
-    console.log(`child process exited with code ${code}`);
-});
+runCommand(angularCliCommand,
+    () => runCommand(ngCreateCommand,
+        () => runCommand(angularMaterialCommand)
+    )
+)
---
+++ types/frontend/angular/src/services/commands.service.ts
@@ -0,0 +1,27 @@
+import { spawn } from "child_process";
+
+export const runCommand = (command: string, callback?: any) => {
+    let isWin = process.platform === "win32";
+    let cmdTerminal;
+
+    if (isWin) {
+        cmdTerminal = spawn('cmd', ['/c', command]);
+    } else {
+        cmdTerminal = spawn(command);
+    }
+
+    cmdTerminal.stdout.on('data', (data: any) => {
+        console.log(`stdout: ${data}`);
+    });
+
+    cmdTerminal.stderr.on('data', (data: any) => {
+        console.log(`stderr: ${data}`);
+    });
+
+    cmdTerminal.on('close', (code: any) => {
+        console.log(`child process exited with code ${code}`);
+        if (callback) {
+            callback();
+        }
+    });
+}
