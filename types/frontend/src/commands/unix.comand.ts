77fd2798a21b54f24a0ab13a58dee5871d8a0538
Germano Loos <germanoloos@hotmail.com>
2021-05-21 11:10:17 -0300

    Execução de linhas de comando pelo node

--- types/frontend/src/commands/unix.comand.ts
+++ types/frontend/src/commands/unix.comand.ts
@@ -1,4 +1,4 @@
-import { ChildProcessWithoutNullStreams, spawn } from "child_process";
+import { spawn } from "child_process";
 import { LoggerRepository } from "../core/repositories/logger.repository";
 import { TerminalCommand } from "./terminal.command";
 
@@ -7,22 +7,20 @@ export class UnixTerminal implements TerminalCommand {
     run = (commands: string[], projectId: string, logger: LoggerRepository): Promise<string> => {
 
         return new Promise((resolve, reject) => {
+            
             try {
-                commands.forEach((command) => {
-                    console.log(command);
-                    const commandSplit = command.split(' ');
-                    const cmdTerminal = spawn(commandSplit[0], commandSplit.slice(1), { shell: true });
-                    cmdTerminal.stdout.on('data', (data: any) => {
-                        logger.info(`${data}`, projectId);
-                    });
+                const cmdTerminal = spawn(commands.join(' && '), [], { shell: true });
 
-                    cmdTerminal.stderr.on('data', (data: any) => {
-                        logger.error(`${data}`, projectId);
-                    });
+                cmdTerminal.stdout.on('data', (data: any) => {
+                    logger.info(`${data}`, projectId);
+                });
 
-                    cmdTerminal.on('close', (code: any) => {
-                        resolve(`child process exited with code ${code}`);
-                    });
+                cmdTerminal.stderr.on('data', (data: any) => {
+                    logger.error(`${data}`, projectId);
+                });
+
+                cmdTerminal.on('close', (code: any) => {
+                    resolve(`child process exited with code ${code}`);
                 });
             } catch (e) {
                 reject(e)
