fe85db0b9a239a00c5f4a2cde19a2fb675f77a4c
Germano Loos <germanoloos@hotmail.com>
2021-05-27 13:27:15 -0300

    Arquivamento de projeto

--- types/frontend/src/index.ts
+++ types/frontend/src/index.ts
@@ -6,7 +6,6 @@ import { ControllerFactory } from "./factories/controller.factory";
 const data = process.argv.slice(2)[0];
 const angurooProject = Base64Adapter.adapt(data);
 
-// ControllerFactory.createProjectController().create(angurooProject).then(() => {
-//     ControllerFactory.arquiveProjectController().arquive(angurooProject)
-// });
-ControllerFactory.arquiveProjectController().arquive(angurooProject)
+ControllerFactory.createProjectController().create(angurooProject).then(() => {
+    ControllerFactory.arquiveProjectController().arquive(angurooProject)
+});
